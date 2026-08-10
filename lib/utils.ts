27d50f1efd7cf83edb3b9370b928ed99e88006a1
samirasoumahoro72-netizen <samira.soumahoro72@gmail.com>
2026-08-10 11:30:38 +0000

    Add lib/utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,21 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(price);
+}
+
+export function formatDate(dateString: string): string {
+  return new Date(dateString).toLocaleDateString('fr-FR', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+}
